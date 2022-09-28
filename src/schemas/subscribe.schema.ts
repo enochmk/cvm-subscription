10f39b7e97eb28d6c619d78f5a10d89e8082b1bf
Enoch Klufio <Enochmk94@gmail.com>
2022-09-28 14:35:21 +0000

    fix: schema for message:

--- src/schemas/subscribe.schema.ts
+++ src/schemas/subscribe.schema.ts
@@ -4,7 +4,7 @@ export const subscribeProductSchema = yup.object({
 	body: yup.object({
 		msisdn: yup.string().min(9).max(10).required(),
 		productID: yup.string().min(3).max(15).required(),
-		message: yup.string().min(4).max(90).required(),
+		message: yup.string().min(4).max(200).required(),
 	}),
 });
 
